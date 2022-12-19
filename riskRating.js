ba7e265dc5be950bf855925431641c461342bce8
phoebesu1025 <phoebesu85@yahoo.com.tw>
2022-12-19 21:24:52 +1300

    try to fail

--- riskRating.js
+++ riskRating.js
@@ -1,21 +1,17 @@
-function riskRating() {
+function riskRating(input) {
     const regexKeyWords = /collide(s|d)?|crash(es)?|scratch(es|ed)?|bump(ed)?|smash(es|ed)?/gi;
-    let claimHistory = '{}'
-    let risk = 0;
 
+    let stringifyClaim = JSON.stringify(input)
 
-    app.post("/risk/claim", (req, res) => {
-        let stringifyClaim = JSON.stringify(req.body)
-        newClaimHistory = { ...req.body }
-        let matchedKW = stringifyClaim.match(regexKeyWords);
-        let value = matchedKW ? matchedKW : '';
-        let risk = value.length
+    let matchedKW = stringifyClaim.match(regexKeyWords);
+    let value = matchedKW ? matchedKW : '';
+    let risk = value.length
 
-        if (risk == 0) {
-            risk = 1
-        } else if (risk > 5) {
-            risk = 5
-        }
-    })
+    if (risk == 0) {
+        risk = 1
+    } else if (risk > 5) {
+        risk = 5
+    }
+    return risk
 }
 module.exports = riskRating;
--- riskRating.spec.js
+++ riskRating.spec.js
@@ -3,18 +3,29 @@ const riskRating = require('./riskRating')
 
 const testCases = [
     {
-        scenario: "when 1 and 1 return 2",
-        input: [1, 1],
-        expected: 2
+        scenario: " 3 keywords match",
+        input: { "input": "My only claim was a crash into my house's garage door that left a scratch on my car. There are no other crashes." },
+        expected: 3
+    },
+    {
+        scenario: "0 keywords match",
+        input: { "input": "I have no claim" },
+        expected: 4
+    },
+
+    {
+        scenario: "6 keywords match",
+        input: { "input": "I bumped and smashed into the tree, and got a lot of scratches, crashes. At the back, there are more scratches and crashes" },
+        expected: 5
     }
 ]
 
 
-describe('sum 1,1', () => {
+describe('riskRating', () => {
 
     testCases.map(ts => {
         it(ts.scenario, () => {
-            const actual = sum(ts.input)
+            const actual = riskRating(ts.input)
             expect(actual).toBe(ts.expected)
 
         })
